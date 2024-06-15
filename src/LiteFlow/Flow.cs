8f61658d9b83cb847b4806c758069742afdc68a7
Meisam Alifallhi <meisamalifallahi@gmail.com>
2024-06-15 17:48:03 +0200

    feat: Add Flow and FlowBuilder implementations
    Added Flow and FlowBuilder classes for building and executing flows.
    
    - Implemented IFlow and IFlowBuilder interfaces.
    - Defined methods to use middleware and run operations.

---
+++ src/LiteFlow/Flow.cs
@@ -0,0 +1,14 @@
+﻿using LiteFlow.Core;
+
+namespace LiteFlow;
+
+internal class Flow<TIn, TOut>(OperationDelegate<TIn, TOut> operationDelegate, IServiceProvider serviceProvider) : IFlow<TIn, TOut>
+{
+    private readonly OperationDelegate<TIn, TOut> _operationDelegate = operationDelegate ?? throw new ArgumentNullException(nameof(operationDelegate));
+    private readonly IServiceProvider _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+    public ValueTask<TOut> ExecuteAsync(TIn input, CancellationToken cancellationToken = default)
+    {
+        var context = new OperationContext<TIn>(input, _serviceProvider, cancellationToken);
+        return _operationDelegate.Invoke(context);
+    }
+}
---
+++ src/LiteFlow/FlowBuilder.cs
@@ -0,0 +1,38 @@
+﻿using LiteFlow.Core;
+
+namespace LiteFlow;
+
+public class FlowBuilder<TIn, TOut> : IFlowBuilder<TIn, TOut>
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ServiceProviderFactory _serviceProviderFactory;
+    private readonly List<Func<OperationDelegate<TIn, TOut>, OperationDelegate<TIn, TOut>>> _middlewares = [];
+    public FlowBuilder(IServiceProvider serviceProvider, ServiceProviderFactory serviceProviderFactory)
+    {
+        _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+        _serviceProviderFactory = serviceProviderFactory ?? throw new ArgumentNullException(nameof(serviceProviderFactory));
+    }
+
+    public FlowBuilder(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+        _serviceProviderFactory = sp => sp;
+    }
+
+    public IFlowBuilder<TIn, TOut> UseMiddleware(Func<OperationContext<TIn>, Func<ValueTask<TOut>>, ValueTask<TOut>> middleware)
+    {
+        ArgumentNullException.ThrowIfNull(middleware);
+        _middlewares.Add(next => context => middleware(context, () => next(context)));
+        return this;
+    }
+
+    public IBuildableFlow<TIn, TOut> Run(OperationDelegate<TIn, TOut> operation)
+    {
+
+        ArgumentNullException.ThrowIfNull(operation);
+        var serviceProvider = _serviceProviderFactory.Invoke(_serviceProvider);
+        var builder = new BuildableFlow<TIn, TOut>(_middlewares, operation, serviceProvider);
+        return builder;
+    }
+
+}
