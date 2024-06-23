21331bada01ae7ece149070646660052416b71ae
Meisam Alifallhi <meisamalifallahi@gmail.com>
2024-06-23 15:20:31 +0200

    refactor: Update program to add service registrations and modify flow builder usage. - Register Handler1, Handler2, Handler3 as singletons. - Refactor flow builder to use middleware and run updated configuration. - Simplify flow execution logic and update example handlers.

--- LiteFlow.Samples/Program.cs
+++ LiteFlow.Samples/Program.cs
@@ -2,75 +2,74 @@
 
 using LiteFlow;
 using LiteFlow.Core;
-using LiteFlow.Extensions;
 using Microsoft.Extensions.DependencyInjection;
+using LiteFlow.Extensions;
 
 Console.WriteLine("Hello, World!");
 var services = new ServiceCollection();
+services.AddSingleton<Handler1>();
+services.AddSingleton<Handler2>();
+services.AddSingleton<Handler3>();
 var sp = services.BuildServiceProvider();
-var builder = new FlowBuilder<Request, Response1>(sp)
-        //.UseMiddleware(async (context, next) =>
-        //    {
-        //        Console.WriteLine(" before s1");
-        //        var result = await next.Invoke();
-        //        Console.WriteLine(" after s1");
-        //        return result;
+var builder = new FlowBuilderProvider<Request, Response1>(sp)
+    .Use(async (context, next) =>
+    {
+        Console.WriteLine(" before s1");
+        var result = await next.Invoke();
+        Console.WriteLine(" after s1");
+        return result;
 
-        //    })
-        //.UseMiddleware(async (context, next) =>
-        //    {
-        //        Console.WriteLine("  before s2");
-        //        var result = await next.Invoke();
-        //        Console.WriteLine("  after s2");
-        //        return result;
-        //    })
-        .Run(Config)
-
+    })
+    .Use(async (context, next) =>
+    {
+        Console.WriteLine("  before s2");
+        var result = await next.Invoke();
+        Console.WriteLine("  after s2");
+        return result;
+    })
+    .Run(b =>b
+        .AddFunction(c=> ValueTask.FromResult(new Response1(1)))
+        .AddFunction((context => ValueTask.FromResult(new Response1(context.Response.Id+1))))
+        .AddAction<Handler1>()
+        .AddFunction<Handler2>()
+        .AddFunction<Handler3>()
+        )
     ;
 
 var flow = builder.Build();
-var result = await flow.ExecuteAsync(new Request());
-Console.WriteLine(result);
+var result1 = await flow.ExecuteAsync(new Request());
+Console.WriteLine(result1);
 return;
 
-OperationDelegate<Request, Response1> Config(IOperationBuilder<Request, Response1> chain)
+Response1 Do(Response1 r)
 {
-    return chain
-        .AddOperation(c=> ValueTask.FromResult(new Response3()))
-        .AddOperation(c => Console.WriteLine("step1"))
-        .AddOperation(c =>
-        {
-            Console.WriteLine("Set Result");
-            return ValueTask.FromResult(new Response1(1));
-        })
-        .AddResultProcessor((_, _, _) =>
-        {
-            Console.WriteLine("Result Processor1");
-        })
-        .AddResultProcessor(_ =>
-        {
-            Console.WriteLine("Result Processor2");
-        })
-        .AddOperation(_ =>
-        {
-            Console.WriteLine("step2");
-            return ValueTask.CompletedTask;
-        })
-        .AddOperation(c=>
-        {
-            Console.WriteLine("Response 2");
-            return ValueTask.FromResult(new Response2());
-        })
-        .AddResultProcessor(c=> Console.WriteLine("Process Response2"))
-        .AddOperation(c=>
-        {
-            Console.WriteLine("Back to response1");
-            return ValueTask.FromResult(new Response1(2));
-        })
-        .Build();
+    return new Response1(r.Id + 1);
 }
 
-
+public class Handler1 : IAction<Request>
+{
+    public ValueTask HandleAsync(Request input, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Handler1");
+        return ValueTask.CompletedTask;
+    }
+}
+public class Handler2 : IFunction<Request,Response1, Response1>
+{
+    public ValueTask<Response1> HandleAsync(Request input, Response1 result, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Handler2");
+        return ValueTask.FromResult(new Response1(Id: result.Id + 1));
+    }
+}
+public class Handler3 : IFunction<Request,Response1, Response1>
+{
+    public ValueTask<Response1> HandleAsync(Request input, Response1 result, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Handler3");
+        return ValueTask.FromResult(new Response1(Id: result.Id + 2));
+    }
+}
 public record Request;
 public record Response1(int Id);
 public record Response2;
