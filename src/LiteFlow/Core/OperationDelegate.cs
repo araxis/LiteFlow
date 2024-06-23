0056cab42434091c3357a30717be34b327085300
Meisam Alifallhi <meisamalifallahi@gmail.com>
2024-06-23 15:16:02 +0200

    refactor: Rename OperationDelegate and restructure delegates. - Rename to OperationAction and OperationFunc for clarity. - Update usages across the project.

--- src/LiteFlow/Core/OperationDelegate.cs
+++ src/LiteFlow/Core/OperationDelegate.cs
@@ -1,5 +1,8 @@
 ﻿namespace LiteFlow.Core;
 
-public delegate ValueTask OperationDelegate<TIn>(OperationContext<TIn> context);
-public delegate ValueTask<TOut> OperationDelegate<TIn, TOut>(OperationContext<TIn> context);
 
+public delegate ValueTask OperationAction<TIn>(OperationContext<TIn> context);
+public delegate ValueTask OperationActionWithResult<TIn, TOut>(OperationContext<TIn, TOut> context);
+
+public delegate ValueTask<TOut> OperationFunc<TIn, TOut>(OperationContext<TIn> context);
+public delegate ValueTask<TOut> OperationFuncWithResult<TIn,TPrev, TOut>(OperationContext<TIn,TPrev> context);
