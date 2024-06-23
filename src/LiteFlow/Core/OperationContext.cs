93faaceadb40df350c9a6378ea5a3a3007970980
Meisam Alifallhi <meisamalifallahi@gmail.com>
2024-06-23 15:16:26 +0200

    refactor: Simplify OperationContext and Context classes. - Merge and streamline context classes for operations. - Add mapping methods for response contexts.

--- src/LiteFlow/Core/OperationContext.cs
+++ src/LiteFlow/Core/OperationContext.cs
@@ -1,15 +1,32 @@
 ﻿namespace LiteFlow.Core;
 
-public class OperationContext<T>(T request, IServiceProvider serviceProvider, CancellationToken cancellationToken)
+public class Context<TIn>(TIn request)
 {
-    public T Request { get; } = request ?? throw new ArgumentNullException(nameof(request));
+    public TIn Request { get; } = request;
+}
+public class Context<TIn, TOut>(TIn request, TOut result)
+{
+    public TIn Request { get; } = request;
+    public TOut Response { get; } = result;
+
+
+}
+public class OperationContext<TRequest>(TRequest request, IServiceProvider serviceProvider, CancellationToken cancellationToken) : Context<TRequest>(request)
+{
     public IServiceProvider ServiceProvider { get; } = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
     public CancellationToken CancellationToken { get; } = cancellationToken;
+
+    public OperationContext<TRequest, TResponse> Map<TResponse>(TResponse response) =>
+        new(Request, response, serviceProvider, CancellationToken);
+
+    public OperationContext<TRequest, TResponse> Map<TResponse>(Func<TResponse> response) => Map(response.Invoke());
 }
-public class OperationContext<TRequest,TResult>(TRequest request,TResult result, IServiceProvider serviceProvider, CancellationToken cancellationToken)
+
+
+public class OperationContext<TRequest, TResponse>(TRequest request, TResponse response, IServiceProvider serviceProvider, CancellationToken cancellationToken) : OperationContext<TRequest>(request, serviceProvider,cancellationToken)
 {
-    public TRequest Request { get; } = request ?? throw new ArgumentNullException(nameof(request));
-    public TResult Result { get; }= result ?? throw new ArgumentNullException(nameof(result));
-    public IServiceProvider ServiceProvider { get; } = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
-    public CancellationToken CancellationToken { get; } = cancellationToken;
+    public TResponse Response { get; } = response;
+
 }
+
+
