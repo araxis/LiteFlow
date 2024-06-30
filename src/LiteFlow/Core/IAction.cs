3996d8b3a67943e581062815a44864ef694dcc39
Meisam Alifallhi <meisamalifallahi@gmail.com>
2024-06-30 11:52:59 +0200

    refactor: rename HandleAsync to ExecuteAsync in IAction interfaces

--- src/LiteFlow/Core/IAction.cs
+++ src/LiteFlow/Core/IAction.cs
@@ -4,10 +4,10 @@
 public interface IAction;
 public interface IAction<in TIn> : IAction
 {
-    ValueTask HandleAsync(TIn input, CancellationToken cancellationToken);
+    ValueTask ExecuteAsync(TIn input, CancellationToken cancellationToken);
 }
 public interface IAction<in TIn, in TOut> : IAction
 {
-    ValueTask HandleAsync(TIn input, TOut result, CancellationToken cancellationToken);
+    ValueTask ExecuteAsync(TIn input, TOut result, CancellationToken cancellationToken);
 }
 
